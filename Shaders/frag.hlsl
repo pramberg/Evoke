465b87ac9c907821c77d5357862d1b60ef5928ee
Viktor Pramberg <46840502+pramberg@users.noreply.github.com>
2020-06-21 18:46:20 +0200

    Removed unused files

--- Shaders/frag.hlsl
+++
@@ -1,4 +0,0 @@
-float4 main(float4 pos : SV_POSITION) : SV_TARGET
-{
-    return float4(pos.xyz * 0.001f, 1);
-}
--- Shaders/vert.hlsl
+++
@@ -1,4 +0,0 @@
-float4 main(float4 InPosition : POSITION) : SV_POSITION
-{
-    return InPosition;
-}
