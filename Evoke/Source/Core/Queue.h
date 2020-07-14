934bbd1a7a04fe13e5f6422dab218b4ab2b237e6
Viktor Pramberg <46840502+pramberg@users.noreply.github.com>
2020-07-14 20:28:30 +0200

    Queue class that can kinda be used async
    Not sure if it's a valid implementation at all though

---
+++ Evoke/Source/Core/Queue.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "Core/Core.h"
+#include <deque>
+#include <type_traits>
+
+// I have no idea if all of this is valid or not...
+enum class EConcurrency
+{
+	Synchronous,
+	Asynchronous,
+};
+
+template<EConcurrency TConcurrency, typename = void>
+class TOptionalConcurrency
+{
+protected:
+	void Lock() { }
+	void Unlock() { }
+};
+
+template<EConcurrency TConcurrency>
+class TOptionalConcurrency<TConcurrency, std::enable_if_t<TConcurrency == EConcurrency::Asynchronous>>
+{
+protected:
+	void Lock() { mMutex.lock(); }
+	void Unlock() { mMutex.unlock(); }
+	std::mutex mMutex;
+};
+
+template<typename T, EConcurrency TConcurrency = EConcurrency::Synchronous>
+class TQueue : public TOptionalConcurrency<TConcurrency>
+{
+public:
+	using Container = std::deque<T>;
+	using ValueType = typename Container::value_type;
+	using ValueRef = typename Container::reference;
+	using ConstValueRef = typename Container::const_reference;
+
+	explicit TQueue() = default;
+	explicit TQueue(std::initializer_list<T> inList) : mContainer(inList) {}
+	explicit TQueue(TQueue<T>&& inQueue) : mContainer(std::move(inQueue.mContainer)) {}
+
+	size_t Count() const { return mContainer.size(); }
+	b8 Empty() const { return mContainer.empty(); }
+
+	ValueRef Front() noexcept { return mContainer.front(); }
+	ConstValueRef Front() const noexcept { return mContainer.front(); }
+	ValueRef Back() noexcept { return mContainer.back(); }
+	ConstValueRef Back() const noexcept { return mContainer.back(); }
+
+	void Push(const ValueType& inValue) { Lock(); mContainer.push_back(inValue); Unlock(); }
+	void Push(ValueType&& inValue) { Lock(); mContainer.push_back(std::move(inValue)); Unlock(); }
+	
+	template<typename... TValues>
+	decltype(auto) Emplace(TValues&&... inValues) { Lock(); mContainer.emplace_back(std::forward<TValues>(inValues)...); Unlock(); }
+
+	void Pop() { Lock(); mContainer.pop_front(); Unlock(); }
+	void Swap(TQueue& inRight) { Lock(); inRight.Lock(); _Swap_adl(mContainer, inRight.mQueue); Unlock(); inRight.Unlock(); }
+
+	typename Container::iterator begin() { return mContainer.begin(); }
+	typename Container::iterator end() { return mContainer.end(); }
+	typename Container::reverse_iterator rbegin() { return mContainer.rbegin(); }
+	typename Container::reverse_iterator rend() { return mContainer.rend(); }
+
+	typename Container::const_iterator begin() const { return mContainer.begin(); }
+	typename Container::const_iterator end() const { return mContainer.end(); }
+	typename Container::const_reverse_iterator rbegin() const { return mContainer.rbegin(); }
+	typename Container::const_reverse_iterator rend() const { return mContainer.rend(); }
+
+private:
+	Container mContainer{};
+};
