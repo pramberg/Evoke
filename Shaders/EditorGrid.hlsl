1d2d99a4da1e36d445b17ee5881468df3f957060
Viktor Pramberg <46840502+pramberg@users.noreply.github.com>
2020-10-11 20:00:26 +0200

    Added some temporary arrows to the grid shader

--- Shaders/EditorGrid.hlsl
+++ Shaders/EditorGrid.hlsl
@@ -16,7 +16,7 @@ struct PixelInput
 PixelInput VSMain(float3 LocalPosition : POSITION)
 {
     PixelInput output;
-    output.WorldPosition = float3(CameraPosition.x, 0.0f, CameraPosition.z) - LocalPosition * 200.0f;
+    output.WorldPosition = float3(CameraPosition.x, 0.0f, CameraPosition.z) - LocalPosition * 400.0f;
     output.Position = mul(float4(output.WorldPosition, 1.0f), ViewProjection);
     return output;
 }
@@ -44,6 +44,22 @@ float3 GetAxes(float3 inUVW, float3 fwidthCos, float line_size)
     return 1.0 - smoothstep(GridLineStart, GridLineEnd, axesDomain - (line_size));
 }
 
+float GetArrow(float2 inUV, float2 inSize, float2 inFwidth)
+{
+    //inUV = inUV - 4.0f * clamp(round(inUV / 4.0f), float2(0.0f, -1.0f), float2(0.0f, 1.0f));
+    inUV /= inFwidth;
+    inSize /= inFwidth;
+    inUV.x = abs(inUV.x);
+    float2 a = inUV - inSize * saturate(dot(inUV, inSize) / dot(inSize, inSize));
+    float2 b = inUV - inSize * float2(saturate(inUV.x / inSize.x), 1.0f);
+    float s = -sign(inSize.y);
+    float2 d = min(float2(dot(a, a), s * (inUV.x * inSize.y - inUV.y * inSize.x)),
+                   float2(dot(b, b), s * (inUV.y - inSize.y)));
+    float sdf = -sqrt(d.x) * sign(d.y);
+    return 1.0f - smoothstep(GridLineStart, GridLineEnd, sdf);
+
+}
+
 float4 PSMain(PixelInput input) : SV_Target
 {
     float3 worldPosFWidth = fwidth(input.WorldPosition);
@@ -55,7 +71,14 @@ float4 PSMain(PixelInput input) : SV_Target
     axesFwidth.y = worldPosFWidth.x;
 
     float grid = GetGrid(abs(input.WorldPosition.xz), worldPosFWidth.xz, 1.0f);
+    float grid2 = GetGrid(abs(input.WorldPosition.xz), worldPosFWidth.xz, 10.0f);
+
     float3 axes = GetAxes(axesDist, axesFwidth, 0.25f);
+
+    float arrowX = GetArrow(input.WorldPosition.zx - float2(0.0f, 3.75), float2(0.15f, -0.5f), worldPosFWidth.zx);
+    float arrowZ = GetArrow(input.WorldPosition.xz - float2(0.0f, 3.75), float2(0.15f, -0.5f), worldPosFWidth.xz);
+    axes.x = max(axes.x, arrowX);
+    axes.y = max(axes.y, arrowZ);
 
     float3 outColor = Color;
     outColor = lerp(outColor, float3(0.7f, 0.2f, 0.3f), axes.x);
@@ -70,6 +93,7 @@ float4 PSMain(PixelInput input) : SV_Target
     angle *= angle;
 
     float fade = 1.0f - angle * angle;
-    fade *= smoothstep(100.0f, 25.0f, distanceToPixel);
+    fade *= smoothstep(200.0f, 25.0f, distanceToPixel);
+    grid = lerp(grid, grid2, smoothstep(0.0f, 150.0f, distanceToPixel));
     return float4(outColor, max(grid, max(axes.x, axes.y)) * fade);
 }
